6d072ad51b3349ab680d7da02a662b1da2ac09f3
ABDALLAH <abdoullahibrahem8@gmail.com>
2024-07-17 12:53:50 +0300

    second commit for remote stage push

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "nice"]}
+    /> */}
+  </React.StrictMode>
+);
